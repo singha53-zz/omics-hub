5e19577d8d985a55c735ff9bb61869b8083e4065
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-11-09 11:57:36 -0500

    repo setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve React build
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// simple API end point
+app.get('/api/msg', (req, res) => {
+  const msg = 'LEARN REACT';
+  res.json(msg)
+});
+
+// catch all handler
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+// listen to port
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
---
+++ test/canary.test.js
@@ -0,0 +1,7 @@
+const expect = require("chai").expect;
+
+describe("canary test", function() {
+  it("should pass this canary test", function() {
+    expect(true).to.be.true;
+  });
+});
