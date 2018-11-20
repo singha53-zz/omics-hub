4b58e684ee3f9ad47b726511d87166974cf783b1
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-11-19 20:46:19 -0500

    update package.json, add concurrenlty, and change port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('*', (req, res) => {
 });
 
 // listen to port
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
