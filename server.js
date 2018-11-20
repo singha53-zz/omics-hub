bb1f6acc537a488d5964dd7ccb026a3826aec7d1
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-11-20 08:30:36 -0500

    update server.js to include mongodb and auth middleware

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User.js');
+require('./services/passport');
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || keys.mongoURI;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
 const app = express();
+
+// make use of cookies in app
+app.use(
+  cookieSession({
+    maxAge: 30 * 24* 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use auth routes
+require('./routes/authRoutes')(app);
 
 // Serve React build
 app.use(express.static(path.join(__dirname, 'client/build')));
